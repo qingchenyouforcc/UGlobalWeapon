de0e3d551909005a1a41abff495cede13e79cf3b
qingchenyouforcc <qingchenformc@outlook.com>
2025-03-06 22:29:37 +0800

    update RPG #3.8

--- src/main/java/org/mmga/uglobal/weapon/RPG.java
+++ src/main/java/org/mmga/uglobal/weapon/RPG.java
@@ -26,7 +26,7 @@ public class RPG extends Fireball {
         fireball.setDirection(direction);
         fireball.setYield(Yield);
         fireball.setMetadata("Weapon", new FixedMetadataValue(Weapon.getInstance(), "Nuclear"));
-        Logger.getLogger("UGlobalWeapon").info("Summoning Nuclear: " + fireball.getMetadata("Weapon") + " at " + location.getX() + "," + location.getY() + "," + location.getZ());
+        Logger.getLogger("UGlobalWeapon").info("Summoning Fireball: " + fireball.getMetadata("Weapon") + " at " + location.getX() + "," + location.getY() + "," + location.getZ());
 
         // 创建一个定时任务，每隔2个 tick 检查火球的位置并生成粒子效果
         new BukkitRunnable() {
